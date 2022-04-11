c4e072799ec8e33caa58362e6cd6e1f65e380ccf
xaro <xabilro@gmail.com>
2022-04-11 17:09:46 +0200

    remove dockerfiles

--- backend/Records/RecordService.cs
+++ backend/Records/RecordService.cs
@@ -2,5 +2,10 @@ namespace backend.Records;
 
 public class RecordService : IRecordService
 {
-    
+    private readonly StuffDbContext _context;
+
+    public RecordService(StuffDbContext context)
+    {
+        _context = context;
+    }
 }
