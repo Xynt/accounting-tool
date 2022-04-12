f8da3707ee8f877e29a9a4e308001c426e656e97
jage <jan.gehbauer@zuehlke.com>
2022-04-12 10:18:24 +0200

    create navbar

--- frontend-2/src/App.tsx
+++ frontend-2/src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import AccountsScreen from './components/AccountsScreen/AccountsScreen';
+import Navbar from './components/Navbar/Navbar';
+import RecordsScreen from './components/RecordsScreen/RecordsScreen';
 
 const App = () => {
   return (
     <div className="App">
-      <AccountsScreen />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/accounts" element={<AccountsScreen />} />
+          <Route path="/records" element={<RecordsScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
